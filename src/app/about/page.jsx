ad3507855ba05cfa3835c2ac2cc84f2bd263d40b
Nancy4510 <nancy4510@gmail.com>
2025-05-29 12:29:27 -0400

    Editing the about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,13 +1,16 @@
 export default function HomePage() {
     return (
         <main>
-            <div>
-                <h1>This is the about page</h1>
+            <div className="hero flex flex-col justify-center content-center">
+                <h1 className="text-center font-bold text-4xl">About</h1>
+                <br></br>
                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat vel libero atque est? Saepe iste, maxime nihil eos tenetur expedita veritatis voluptates eveniet quos distinctio. Aperiam eaque repudiandae minus sequi.</p>
             </div>
-
+            <br></br>
             <div>
-                <h2>Meet the team</h2>
+                <h2 className="text-center font-bold text-2xl">Meet the team</h2>
+                <br></br>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, laborum totam suscipit repellat a autem minus voluptatem quaerat incidunt aut. In minima accusantium reiciendis porro laborum delectus. Fugit, vitae repellat.</p>
             </div>
         </main>
     )
