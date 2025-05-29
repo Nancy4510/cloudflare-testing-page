9c85f3e94913547b631a77e8c6ecb1eca142f0d2
Nancy4510 <nancy4510@gmail.com>
2025-05-29 11:22:14 -0400

    Started new project

---
+++ src/app/about/page.jsx
@@ -0,0 +1,14 @@
+export default function HomePage() {
+    return (
+        <main>
+            <div>
+                <h1>This is the about page</h1>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat vel libero atque est? Saepe iste, maxime nihil eos tenetur expedita veritatis voluptates eveniet quos distinctio. Aperiam eaque repudiandae minus sequi.</p>
+            </div>
+
+            <div>
+                <h2>Meet the team</h2>
+            </div>
+        </main>
+    )
+}
