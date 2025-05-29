9cd7912cedd203df34c67007ef7473fcbd830095
Nancy4510 <nancy4510@gmail.com>
2025-05-29 13:01:26 -0400

    Working on adding more content and pages

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,17 +1,23 @@
-export default function HomePage() {
+export default function AboutPage() {
     return (
         <main>
-            <div className="hero flex flex-col justify-center content-center">
+            <div className="hero flex flex-col items-center border border-solid border-red-500">
                 <h1 className="text-center font-bold text-4xl">About</h1>
                 <br></br>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat vel libero atque est? Saepe iste, maxime nihil eos tenetur expedita veritatis voluptates eveniet quos distinctio. Aperiam eaque repudiandae minus sequi.</p>
+                <div className="justify-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat vel libero atque est? Saepe iste, maxime nihil eos tenetur expedita veritatis voluptates eveniet quos distinctio. Aperiam eaque repudiandae minus sequi.</div>
             </div>
             <br></br>
             <div>
                 <h2 className="text-center font-bold text-2xl">Meet the team</h2>
                 <br></br>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, laborum totam suscipit repellat a autem minus voluptatem quaerat incidunt aut. In minima accusantium reiciendis porro laborum delectus. Fugit, vitae repellat.</p>
+                <br></br>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque voluptatem inventore et obcaecati, veniam ipsam optio doloremque quis commodi doloribus saepe architecto odio est illum, dolorem, eaque necessitatibus accusantium iusto.</p>
+                <br></br>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit eveniet sunt nihil explicabo impedit consequatur ducimus qui, deleniti rerum dolor, ratione veniam harum excepturi doloremque non! Laborum tempore labore blanditiis?</p>
             </div>
+
+
         </main>
     )
 }
