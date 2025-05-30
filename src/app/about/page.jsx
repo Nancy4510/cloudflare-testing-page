4ace0b30a9cb8d29928dd15fd16d8d636783cd34
Nancy4510 <nancy4510@gmail.com>
2025-05-30 15:44:32 -0400

    Added images

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,27 +3,35 @@ import Image from "next/image";
 export default function AboutPage() {
     return (
         <main>
-            <div className="hero flex flex-col border border-solid border-red-500 m-auto">
+            <div className="hero flex flex-col border border-solid border-red-500">
                 <h1 className="text-center font-bold text-4xl">About</h1>
                 <br></br>
                 <div className="justify-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat vel libero atque est? Saepe iste, maxime nihil eos tenetur expedita veritatis voluptates eveniet quos distinctio. Aperiam eaque repudiandae minus sequi.</div>
             </div>
-            <br></br>
-            <div>
-                <h2 className="text-center font-bold text-2xl">Meet the team</h2>
-                <br></br>
-                <Image 
-                className="border border-solid border-red-500"
-                src="/imgs/jane-smith.jpg"
-                alt="A profile picture of Jane Smith"
-                width={300}
-                height={300}
-                />
 
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, laborum totam suscipit repellat a autem minus voluptatem quaerat incidunt aut. In minima accusantium reiciendis porro laborum delectus. Fugit, vitae repellat.</p>
+            <br></br>
+
+            <div className="flex">
+            <h2 className="text-center font-bold text-2xl">Meet the team</h2>
+
                 <br></br>
+                
+                <div className="border border-solid border-red-500">
+                    <Image 
+                    className="h-90 object-contain"
+                    src="/imgs/jane-smith.jpg"
+                    alt="A profile picture of Jane Smith"
+                    width={300}
+                    height={300}
+                    />
+
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, laborum totam suscipit repellat a autem minus voluptatem quaerat incidunt aut. In minima accusantium reiciendis porro laborum delectus. Fugit, vitae repellat.</p>
+                </div>
+
+                <br></br>
+
                 <Image 
-                className="border border-solid border-red-500"
+                className=""
                 src="/imgs/diego-hernandez.jpg"
                 alt="A profile picture of Diego Hernandez"
                 width={300}
@@ -32,7 +40,7 @@ export default function AboutPage() {
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque voluptatem inventore et obcaecati, veniam ipsam optio doloremque quis commodi doloribus saepe architecto odio est illum, dolorem, eaque necessitatibus accusantium iusto.</p>
                 <br></br>
                 <Image 
-                className="border border-solid border-red-500"
+                className=""
                 src="/imgs/puggster-jones.jpg"
                 alt="A profile picture of a dog who is a pug and the mascot."
                 width={300}
